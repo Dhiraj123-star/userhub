aa678f0a0365a5a1f9845ae1c4d463abcdcee10b
Dhiraj Kumar <gdhiraj499@gmail.com>
2025-08-23 23:52:11 +0530

    init: configure MariaDB connection with environment variables

---
+++ userhub/backend/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+# Database URL from environment variable
+DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://dbuser:dbpass@localhost:3306/userhub")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
